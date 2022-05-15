dd74c569726ed168604912ca0df1904e97d96cae
Otabek0302 <fenix_systems@MacBook-Pro.local>
2022-05-16 00:37:54 +0500

    I add my files

---
+++ main.js
@@ -0,0 +1,78 @@
+// let age = prompt('How old are you ?');
+// console.log(age)
+
+// if (age >= 50) {
+//      document.getElementById('demo').innerHTML = "пора копить деньги на гроб"
+// } else if (age >= 25){
+//      document.getElementById('demo').innerHTML = " пора жениться"
+// } else if (age >= 16) {
+//      document.getElementById('demo').innerHTML = "юноша"
+// } else if (age >= 12) {
+//      document.getElementById('demo').innerHTML = "маленький"
+// } else {
+//      document.getElementById('demo').innerHTML = "ребенок"
+// }
+
+
+// let months = prompt('Write Manth Name');
+// console.log(months)
+
+// if (months === 'January') {
+//      document.getElementById('demo').innerHTML = "Winter"
+// } else if (months === 'February'){
+//      document.getElementById('demo').innerHTML = "Winter"
+// } else if (months === 'March') {
+//      document.getElementById('demo').innerHTML = "Spring"
+// } else if (months === 'April') {
+//      document.getElementById('demo').innerHTML = "Spring"
+// } else if (months === 'May') {
+//      document.getElementById('demo').innerHTML = "Spring"
+// } else if (months === 'June') {
+//      document.getElementById('demo').innerHTML = "Summer"
+// } else if (months === 'July') {
+//      document.getElementById('demo').innerHTML = "Summer"
+// } else if (months === 'August') {
+//      document.getElementById('demo').innerHTML = "Summer"
+// } else if (months === 'September') {
+//      document.getElementById('demo').innerHTML = "Autumn"
+// } else if (months === 'October') {
+//      document.getElementById('demo').innerHTML = "Autumn"
+// } else if (months === 'November') {
+//      document.getElementById('demo').innerHTML = "Autumn"
+// } else if (months === 'December') {
+//      document.getElementById('demo').innerHTML = "Winter"
+// } else {
+//      document.getElementById('demo').innerHTML = "There no month by the name like this"
+// }
+
+let number = parseInt(prompt('Choose number beetwen from 1 to 12'));
+console.log(number)
+if (number === 1) {
+     document.getElementById('demo').innerHTML = "January"
+} else if (number === 2){
+     document.getElementById('demo').innerHTML = "February"
+} else if (number === 3) {
+     document.getElement1d('demo').innerHTML = "March"
+} else if (number === 4) {
+     document.getElementById('demo').innerHTML = "April"
+} else if (number === 5) {
+     document.getElementById('demo').innerHTML = "May"
+} else if (number === 6) {
+     document.getElementById('demo').innerHTML = "June"
+} else if (number === 7) {
+     document.getElementById('demo').innerHTML = "July"
+} else if (number === 8) {
+     document.getElementById('demo').innerHTML = "August"
+} else if (number === 9) {
+     document.getElementById('demo').innerHTML = "September"
+} else if (number === 10) {
+     document.getElementById('demo').innerHTML = "October"
+} else if (number === 11) {
+     document.getElementById('demo').innerHTML = "November"
+} else if (number === 12) {
+     document.getElementById('demo').innerHTML = "December"
+} else {
+     document.getElementById('demo').innerHTML = "There no month by the name like this"
+}
+
+
